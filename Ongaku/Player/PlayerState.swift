6fe6262cab7ee23c1ba88c073c6abea18d327bee
slice <tinyslices@gmail.com>
2023-06-20 12:06:40 -0700

    Refactor the store URL injection logic
    I was talking about Swift pattern matching to some friends and they
    pointed out that this code could be a bit cleaner.

--- Ongaku/Player/PlayerState.swift
+++ Ongaku/Player/PlayerState.swift
@@ -13,14 +13,30 @@ enum PlayerState {
         var track: Track
         let position: TimeInterval
     }
-
+    
     /// Indicates that the player was playing a track, but has been paused by
     /// the user.
     case paused(Active)
-
+    
     /// Indicates that the player is actively playing a track.
     case playing(Active)
-
+    
     /// Indicates that the player is not playing anything.
     case stopped
+}
+
+extension PlayerState {
+    /// Returns whether the player is actively playing a track.
+    var isPlaying: Bool {
+        switch self {
+        case .playing: return true
+        default: return false
+        }
+    }
+
+    /// Returns whether the player was playing a track, but has been paused by
+    /// the user.
+    var isPaused: Bool {
+        !isPlaying
+    }
 }

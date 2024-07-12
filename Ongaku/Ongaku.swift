f0ae459af8b0f44e1ee604f2ec023e4b8127e20c
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2024-07-11 19:29:41 -0500

    UI update; switch to control center-like
    Using orchetect/MacControlCenterUI

--- Ongaku/Ongaku.swift
+++ Ongaku/Ongaku.swift
@@ -9,11 +9,16 @@
 import Combine
 import os.log
 import SwiftUI
+import MacControlCenterUI
 
 private let log: Logger = .init(subsystem: "io.github.spotlightishere.Ongaku", category: "main-app")
 
 @main
 struct Ongaku: App {
+    @State var isMenuPresented: Bool = false
+    let blurple: Color = Color(red: 0.34375, green: 0.39453125, blue: 0.9453125)
+    let lastFmColor: Color = Color(red: 0.83203125, green: 0.06640625, blue: 0.02734375)
+    
     init() {
         do {
             player = try MusicPlayer()
@@ -31,46 +36,108 @@ struct Ongaku: App {
     let player: Player
     var playerSink: AnyCancellable?
     let rpc: RPCController
+    @State var discord: Bool = true
     @StateObject var scrobbler: ScrobblerController
 
     var body: some Scene {
         // Designed to match the style of the default
         // Storyboard-based menu items.
         MenuBarExtra("Ongaku", image: "status_icon") {
-            Button("About Ongaku") {
-                NSApplication.shared.orderFrontStandardAboutPanel()
-            }
-
-            Divider()
-
-            if let session = scrobbler.session {
-                Text("Logged in as \(session.name)")
-
-                Toggle(isOn: $scrobbler.enabled) {
-                    if scrobbler.enabled { Text("Scrobbling") } else { Text("Not scrobbling") }
-                }.keyboardShortcut("s", modifiers: [.command])
-
-                Button("Logout") {
-                    do {
-                        try scrobbler.clearSession()
-                    } catch {
-                        log.error("Error clearing session: \(error)")
+            
+            MacControlCenterMenu(isPresented: $isMenuPresented) {
+                MenuHeader("Ongaku") {
+                    Text(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String)
+                        .foregroundColor(.secondary)
+                }
+                
+                MenuSection("Presence")
+                
+                HStack {
+                    MenuCircleToggle(
+                        isOn: $discord,
+                        controlSize: .prominent,
+                        style: .init(
+                            image: Image("discord"),
+                            color: blurple
+                        )
+                    ) { Text("Discord") }
+                    MenuCircleToggle(
+                        isOn: $scrobbler.enabled,
+                        controlSize: .prominent,
+                        style: .init(
+                            image: Image("last_fm"),
+                            color: lastFmColor
+                        )
+                    ) { Text("Last.fm") } onClick: { toggle in
+                        print(toggle)
                     }
-                }.keyboardShortcut("l", modifiers: [.command])
-            } else {
-                Button("Login to Last.fm") {
-                    Task(priority: .userInitiated) {
-                        await scrobbler.fetchAndSaveSession()
-                        scrobbler.enabled = true
+                }
+                .frame(height: 80)
+                
+                MenuDisclosureSection("Last.fm", initiallyExpanded: false) {
+                    if let session = scrobbler.session {
+                        MenuCommand {} label: {
+                            HStack {
+                                Image(systemName: "person.crop.circle.fill")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(2)
+                                Text("\(session.name)")
+                                Spacer()
+                            }
+                        }
+                        
+                        MenuCommand {
+                            do {
+                                try scrobbler.clearSession()
+                            } catch {
+                                log.error("Error clearing session: \(error)")
+                            }
+                        } label: {
+                            HStack {
+                                Image(systemName: "rectangle.portrait.and.arrow.right")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(2)
+                                Text("Logout")
+                                Spacer()
+                            }
+                        }
+                    } else {
+                        MenuCommand {
+                            Task(priority: .userInitiated) {
+                                await scrobbler.fetchAndSaveSession()
+                                scrobbler.enabled = true
+                            }
+                        } label: {
+                            HStack {
+                                Image("last_fm")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .padding(2)
+                                    .foregroundColor(lastFmColor)
+                                Text("Login to Last.fm")
+                                Spacer()
+                            }
+                        }
                     }
-                }.keyboardShortcut("l", modifiers: [.command])
+                }
+                
+                Divider()
+
+                MenuCommand {
+                    NSApplication.shared.orderFrontStandardAboutPanel()
+                    NSApplication.shared.activate(ignoringOtherApps: true)
+                } label: {
+                    Text("About")
+                }
+
+                Divider()
+
+                MenuCommand("Quit") {
+                    NSApplication.shared.terminate(nil)
+                }
             }
-
-            Divider()
-
-            Button("Quit Ongaku") {
-                NSApplication.shared.terminate(nil)
-            }.keyboardShortcut("q", modifiers: [.command])
-        }.menuBarExtraStyle(.menu)
+        }.menuBarExtraStyle(.window)
     }
 }
